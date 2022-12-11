87cbccbb62501e013806c8d2360c828180bf028f
Yair Lázaro <yairlazaro@outlook.com>
2022-12-11 03:20:32 -0600

    edit: reacomodo de código con prettier

--- src/server/trpc/router/userAuth.ts
+++ src/server/trpc/router/userAuth.ts
@@ -1,26 +1,32 @@
-import {z} from "zod";
+import { z } from "zod";
 import { router, publicProcedure } from "../trpc";
 
-import {saveUserController} from "server/controllers/authUser/saveUser";
-import {verifyAuthStateController} from "server/controllers/authUser/verifyAuthState";
-import {generateTokenController} from "server/controllers/globalControllers/generateToken";
+import { saveUserController } from "server/controllers/authUser/saveUser";
+import { verifyAuthStateController } from "server/controllers/authUser/verifyAuthState";
+import { generateTokenController } from "server/controllers/globalControllers/generateToken";
 
 export const userAuth = router({
   saveUser: publicProcedure
-    .input(z.object({ 
-      name: z.string(),
-      email: z.string().email(),
-      password: z.string(),
-    }))
-    .query( saveUserController ),
+    .input(
+      z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string(),
+      })
+    )
+    .query(saveUserController),
   generateToken: publicProcedure
-    .input(z.object({
-      id: z.string(),
-    }))
-    .query( generateTokenController ),
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query(generateTokenController),
   verifyAuth: publicProcedure
-    .input(z.object({
-      jwt: z.string(),
-    }))
-    .query( verifyAuthStateController ),
+    .input(
+      z.object({
+        jwt: z.string(),
+      })
+    )
+    .query(verifyAuthStateController),
 });
