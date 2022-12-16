372dde564fb6eace38e0ff387e764c372171b988
Yair Lázaro <yairlazaro@outlook.com>
2022-12-16 02:16:26 -0600

    edit: input de tipo text por uno de tipo pasword y se añadió un pequeño texto al final para redireccionar a la página de singin

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,16 @@
+import { InputPassword } from "components/Form/InputPassword";
 import { InputText } from "components/Form/InputText";
 import { Submit } from "components/Form/Submit";
+import { LinkText } from "components/Links/LinkText";
 import { FormProvider } from "context/form/FormProvider";
 import { z } from "zod";
 
 const loginInit = {
-  name: "",
   email: "",
   password: "",
 };
 
 const validation = {
-  name: z.string().min(4),
   email: z.string().email(),
   password: z.string().min(8),
 };
@@ -30,22 +30,16 @@ const Login = () => {
           }}
           className="m-2 w-full max-w-[28rem] p-4"
         >
-          <h2 className="mb-4 border-l-4 border-cyan-500 pl-10 text-xl font-bold text-neutral-400">
+          <h2 className="mb-4 border-l-4 border-cyan-500 pl-10 text-xl font-bold text-neutral-500">
             LOGIN
           </h2>
-          <InputText
-            className="mb-2 w-full bg-neutral-100 p-4 text-neutral-500 outline-none"
-            name="name"
-            alert="El nombre es requerido y de mínimo 4 carácteres"
-            placeholder="Nombre:"
-          />
           <InputText
             className="mb-2 w-full bg-neutral-100 p-4 text-neutral-500 outline-none"
             name="email"
             alert="Coloca un email válido"
             placeholder="Email:"
           />
-          <InputText
+          <InputPassword
             className="mb-2 w-full bg-neutral-100 p-4 text-neutral-500 outline-none"
             name="password"
             alert="La contraseña debe contener al menos 8 carácteres"
@@ -53,8 +47,11 @@ const Login = () => {
           />
           <Submit
             className="mb-2 w-full bg-cyan-500 p-4 text-xl font-semibold text-neutral-200 hover:opacity-80 active:bg-cyan-600"
-            text="Login"
+            text="Iniciar sesión"
           />
+          <p className="text-neutral-500">
+            ¿No tienes una cuenta? <LinkText text="Crea una" href="/singin" />.
+          </p>
         </FormProvider>
       </section>
     </main>
