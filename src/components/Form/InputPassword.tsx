726917c5552278101cf6fd1eaca415cbbaae820f
Yair Lázaro <yairlazaro@outlook.com>
2022-12-16 02:13:06 -0600

    add: componente input de tipo password para el formulario

---
+++ src/components/Form/InputPassword.tsx
@@ -0,0 +1,51 @@
+import { BLUR, INPUT_STATE } from "CONSTANTS/FORM";
+import { FormContext } from "context/form/FormContext";
+import { useInputValidation } from "hooks/forms/useInputValidation";
+import { BaseSyntheticEvent, useContext } from "react";
+import { Alert } from "./Alert";
+import { Label } from "./Label";
+
+export interface InputTextProps {
+  name: string;
+  alert: string;
+  label?: string;
+  placeholder?: string;
+  className?: string;
+}
+
+export const InputPassword = ({
+  name,
+  alert,
+  label,
+  placeholder,
+  className = "",
+}: InputTextProps) => {
+  const { setInputForm, form } = useContext(FormContext);
+
+  const { onFocusIn, onFocusOut, onBlur, isValid } = useInputValidation({
+    inputName: name,
+  });
+
+  const change = (e: BaseSyntheticEvent) => {
+    setInputForm(name, e.target.value);
+  };
+
+  return (
+    <div>
+      {label && <Label htmlFor={name} text={label} />}
+      <input
+        id={name}
+        className={className}
+        type="password"
+        name={name}
+        value={form[name]}
+        placeholder={placeholder}
+        onChange={change}
+        onFocus={onFocusIn}
+        onBlur={onFocusOut}
+      />
+      {onBlur.touch === BLUR.IS_TOUCHED &&
+        isValid === INPUT_STATE.IS_INVALID && <Alert text={alert} />}
+    </div>
+  );
+};
