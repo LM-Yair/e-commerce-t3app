1d83224e31cf15694663a5da2e6ecc03b5e80696
Yair Lázaro <yairlazaro@outlook.com>
2022-12-17 04:56:58 -0600

    add: método para logear un usuario cuando este ya existe

--- src/server/trpc/router/userAuth.ts
+++ src/server/trpc/router/userAuth.ts
@@ -4,6 +4,7 @@ import { router, publicProcedure } from "../trpc";
 import { registerUserController } from "server/controllers/authUser/registerUser";
 import { verifyAuthStateController } from "server/controllers/authUser/verifyAuthState";
 import { generateTokenController } from "server/controllers/globalControllers/generateToken";
+import { loginUserController } from "server/controllers/authUser/loginUser";
 
 export const userAuth = router({
   registerUser: publicProcedure
@@ -15,6 +16,14 @@ export const userAuth = router({
       })
     )
     .query(registerUserController),
+  loginUser: publicProcedure
+    .input(
+      z.object({
+        email: z.string().email(),
+        password: z.string(),
+      })
+    )
+    .query(loginUserController),
   generateToken: publicProcedure
     .input(
       z.object({
