d1058c1340d19cc0fbf42ae0be11f85a2b650dfa
Yair Lázaro <yairlazaro@outlook.com>
2022-12-20 04:27:12 -0600

    add: se agregó la sección para listar los productos creados por los usuarios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,11 @@
 import Head from "next/head";
 import { type NextPage } from "next";
-import { trpc } from "utils/trpc";
 
 import { PageLayout } from "components/Pages/PageLayout";
-import { Product } from "components/Cards/Product";
 import { AuthProvider } from "context/auth/AuthProvider";
+import { LastProducts } from "components/Sections/LastProducts";
 
 const Home: NextPage = () => {
-  // const user = trpc.userAuth.saveUser.useQuery({
-  //   name: 'Yair Lázaro',
-  //   email: 'yairlazaro@outlook.com',
-  //   password: '12345',
-  // });
-  // console.log( 'Ususario guardado: ', user);
-  // const id = "d9cfe65f-1c1e-4ab7-a531-a0606e91b779";
-  // const token = trpc.userAuth.generateToken.useQuery({id});
-  // console.log('Token creado: ', token);
-  // const token = {
-  //   jwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQ5Y2ZlNjVmLTFjMWUtNGFiNy1hNTMxLWEwNjA2ZTkxYjc3OSIsImlhdCI6MTY2OTMzODAxMSwiZXhwIjoxNjY5NDI0NDExfQ.8uCmld0G0LxbnQ0yG_e94uaS_MdFdoBdeRV47fV_zeo"
-  // }
-  // const tokenState = trpc.userAuth.verifyAuth.useQuery(token);
-  // console.log('Token status: ', tokenState);
-
   return (
     <AuthProvider>
       <PageLayout>
@@ -31,14 +15,7 @@ const Home: NextPage = () => {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <main>
-          <section className="mx-auto flex max-w-4xl flex-wrap justify-evenly gap-2 p-2">
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-          </section>
+          <LastProducts />
         </main>
       </PageLayout>
     </AuthProvider>
