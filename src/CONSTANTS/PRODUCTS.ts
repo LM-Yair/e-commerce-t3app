0dd79fe5d3ddc21aed4b670b036d1111eae27384
Yair Lázaro <yairlazaro@outlook.com>
2022-12-20 04:24:51 -0600

    add: constantes para el manejo del estado en los productos

---
+++ src/CONSTANTS/PRODUCTS.ts
@@ -0,0 +1,13 @@
+/*
+ * Constantes para manejar el estado de los  productos
+ * al momento de hacer la petición a la base de datos
+*/
+
+export const PRODUCTS = {
+  INITIAL_STATE:0,
+  LOADING: 1,
+  ERROR: -1,
+  DONE: 2,
+}
+
+export const PRODUCT = PRODUCTS; 
