184b9c78f792eb0752d4ef0d547c4ec315002aa3
Yair Lázaro <yairlazaro@outlook.com>
2022-12-29 01:58:10 -0600

    edit: se removió el icono del carrito

--- src/components/Cards/Product.tsx
+++ src/components/Cards/Product.tsx
@@ -1,4 +1,3 @@
-import { CartIcon } from "components/Icons/Cart";
 import { LinkText } from "components/Links/LinkText";
 import { Product } from "interfaces/product/product";
 
@@ -9,18 +8,15 @@ type ProductCardProps = Omit<
 
 export const Productcard = ({ name, price, slug }: ProductCardProps) => {
   return (
-    <article className="inline-block w-56 rounded-lg bg-neutral-100 p-2 text-neutral-800 shadow-md shadow-neutral-300">
+    <article className="inline-block w-56 rounded-lg bg-neutral-100 p-2.5 text-neutral-800 shadow-md shadow-neutral-300">
       <div className="h-48 rounded-lg bg-neutral-300"></div>
       <div className="flex flex-col">
-        <p className="py-1 text-lg font-bold">
+        <p className="py-1 text-lg font-bold text-neutral-800">
           <LinkText type="NextLink" text={name} href={`/producto/${slug}`} />
         </p>
         <ul className="pl-2">
           <li>MXN {price}</li>
         </ul>
-      </div>
-      <div className="flex flex-wrap gap-2 pt-2">
-        <CartIcon size={24} action={() => console.log("cart")} />
       </div>
     </article>
   );
